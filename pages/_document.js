87c00df5ec4037dc7b6f95e67a148c726fc29f90
Maximilian Jendrall <hello@max.berlin>
2020-09-27 23:46:41 +0200

    Fix: dark mode theme

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="">
+        <body className="bg-black text-white">
           <Main />
           <NextScript />
         </body>
