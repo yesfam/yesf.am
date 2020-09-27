ff62cc83a78a99f9141b29f60ed0858d592cef56
Maximilian Jendrall <hello@max.berlin>
2020-09-27 23:47:13 +0200

    Add: fathom tracker

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, Head, NextScript } from 'next/document'
 import Footer from "components/footer"
 
 export default class MyDocument extends Document {
@@ -10,6 +10,20 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(f, a, t, h, o, m){
+          	a[h]=a[h]||function(){
+          		(a[h].q=a[h].q||[]).push(arguments)
+          	};
+          	o=f.createElement('script'),
+          	m=f.getElementsByTagName('script')[0];
+          	o.async=1; o.src=t; o.id='fathom-script';
+          	m.parentNode.insertBefore(o,m)
+          })(document, window, '//analytics.max.berlin/tracker.js', 'fathom');
+          fathom('set', 'siteId', 'FHDLF');
+          fathom('trackPageview');
+        `}} />
       </Html>
     )
   }
