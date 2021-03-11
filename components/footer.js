09dbe8772e3dc980293a490d67fb582db750de97
Maximilian Jendrall <hello@max.berlin>
2021-03-11 13:04:57 +0100

    Fix: year, email

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-xs my-16 text-center">
-      <p>&copy; 2020 - architects@yes.fam</p>
+      <p>&copy; 2021 - architects@yesf.am</p>
       <p>We use anonymous analytics.</p>
     </footer>
   )
